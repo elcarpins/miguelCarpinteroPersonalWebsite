4222b134574a59222aa9e046b00363c9c1b3eb14
elcarpins <miguelcarpintero93@gmail.com>
2021-10-13 20:04:13 +0200

    files list completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//===================================== DARK THEME =========================
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// PREVEIOSLY SELECTED TOPIC (checking from local storage)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
